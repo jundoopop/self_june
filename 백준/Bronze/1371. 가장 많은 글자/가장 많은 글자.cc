0ad1422d772577102bb6dc9dbf9a7452934b3b38
권준혁 / Kwon Joonhyeok <45958283+jundoopop@users.noreply.github.com>
2023-12-28 02:51:47 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.cc
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,30 +7,30 @@
 int main(void) {
   // Contains the number of alphabets in each sentences,
   // the number of counted a to z by indices
-  std::vector<int> frequencyOfAlphabets = {0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                           0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                           0, 0, 0, 0, 0, 0, 0, 0};
+  std::vector<int> frequencyOfAlphabets(26, 0);
 
   std::string sentence; // Contains the sentence to be inputted
 
   for (int i = 0; i < 50; i++) // For each sentence
   {
-    getline(std::cin, sentence);    // Input the sentence
+    getline(std::cin, sentence); // Input the sentence
+
     for (auto character : sentence) // For each character in the sentence
     {
-      if ((int)character >= 97 && (int)character <= 133)
+      if (isalpha(character))
         frequencyOfAlphabets[character -
                              'a']++; // Increase the number of alphabets by 1
     }
   }
 
-  const int mostFrequentAlphabet = *std::max_element(
+  const int mostFrequentAlphabetCount = *std::max_element(
       frequencyOfAlphabets.begin(),
       frequencyOfAlphabets.end()); // Contains the most frequent alphabet
 
   for (int i = 0; i < 26; i++) { // For each alphabet
     if (frequencyOfAlphabets.at(i) ==
-        mostFrequentAlphabet) // If the number of alphabets is the most frequent
+        mostFrequentAlphabetCount)  // If the number of alphabets is the most
+                                    // frequent
       std::cout << (char)(i + 'a'); // Print the alphabet
   }
 
