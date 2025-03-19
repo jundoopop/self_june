aa426d6becc3221c269770ccf7c64174e755d40b
권준혁 / Kwon Joonhyeok <45958283+jundoopop@users.noreply.github.com>
2025-03-19 17:37:19 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -3,24 +3,21 @@
 
 int main() {
 
-  // Declare a vector for the values of dp
-  // dp[0] = 0, dp[1] = 1, dp[2] = 2, dp[3] = 3
-  std::vector<int> dp = {0, 1, 2, 3};
-
-  // input
+  // Input
   int n;
   std::cin >> n;
 
-  // if (n < 1 || n > 1000) {
-  //   std::cerr << "Input out of range\n";
-  //   return 1;
-  // }
+  // Declare a vector for the values of dp
+  std::vector<int> dp(n + 1);
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 3;
 
   // If n is larger than 3, use dp formula
-  // dp[n] = dp[n-2] * 2 + dp[n-1]
   if (n > 3) {
-    for (int i = 4; i < n + 1; i++) {
-      dp.push_back((dp[i - 2] + dp[i - 1]) % 10007);
+    for (int i = 4; i <= n; i++) {
+      dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
     }
   }
 
