84d5e4aca1f5e3abbebcdc6e5920359ce79ebf18
권준혁 / Kwon Joonhyeok <45958283+jundoopop@users.noreply.github.com>
2025-03-29 12:59:19 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -2,23 +2,26 @@
 
 typedef unsigned int uint;
 
-int countSetBits(uint number) {
-    int count = 0;
-    while (number > 0) {
-        count += number & 1;  // Check if the least significant bit is 1
-        number >>= 1;         // Right shift the number by 1 bit
-    }
-    return count;
+int countSetBits(uint number)
+{
+  int count = 0;
+  while (number > 0)
+  {
+    count += number & 1; // Check if the least significant bit is 1
+    number >>= 1;        // Right shift the number by 1 bit
+  }
+  return count;
 }
 
-int main() {
-    uint target;
-    std::cin >> target;
+int main()
+{
+  uint target;
+  std::cin >> target;
 
-    // Count the number of set bits in the target length
-    int result = countSetBits(target);
+  // Count the number of set bits in the target length
+  int result = countSetBits(target);
 
-    std::cout << result << std::endl;
+  std::cout << result << std::endl;
 
-    return 0;
+  return 0;
 }
