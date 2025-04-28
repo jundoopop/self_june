ff20bfc4654b23ebf5d01745dfdb323ca54b43ea
권준혁 / Kwon Joonhyeok <45958283+jundoopop@users.noreply.github.com>
2025-04-29 01:17:36 +0900

    [Silver V] Title: 집합, Time: 560 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,40 +1,55 @@
 #include <iostream>
 #include <string>
-#include <unordered_set>
 
-int main() {
+int main()
+{
+    // IO performance
     std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
 
-    std::unordered_set<int> S;
-    int iterateNum;
-    std::cin >> iterateNum;
+    // Number to iterate
+    int M;
+    std::cin >> M;
 
-    for (int i = 0; i < iterateNum; ++i) {
-        std::string command;
-        int x = 0;
-        std::cin >> command;
+    std::string operation;
+    int setBit = 0; // Bitmask should be initialised as zero
 
-        if (command == "add" || command == "remove" || command == "check" || command == "toggle") {
+    while (M-- > 0) // Loop
+    {
+        std::cin >> operation; // Choose one of six set methods
+        int x;
+
+        if (operation == "add")
+        {
             std::cin >> x;
+            setBit |= (1 << x);
         }
-
-        if (command == "add") {
-            S.insert(x);
-        } else if (command == "remove") {
-            S.erase(x);
-        } else if (command == "check") {
-            std::cout << S.count(x) << '\n';
-        } else if (command == "toggle") {
-            if (S.count(x)) {
-                S.erase(x);
-            } else {
-                S.insert(x);
-            }
-        } else if (command == "all") {
-            S = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        } else if (command == "empty") {
-            S.clear();
+        else if (operation == "remove")
+        {
+            std::cin >> x;
+            setBit &= ~(1 << x); // Clearing a bit
+        }
+        else if (operation == "check")
+        {
+            std::cin >> x;
+            // Bitwise AND
+            // Prints 1 if both are 1, 0 if one of them is 0
+            std::cout << (setBit & (1 << x) ? 1 : 0) << '\n';
+        }
+        else if (operation == "toggle")
+        {
+            std::cin >> x;
+            setBit ^= (1 << x); // XOR
+        }
+        else if (operation == "all")
+        {
+            // 1.....0_2 - 1_2 = 1111...11_2
+            setBit = (1 << 21) - 1;
+        }
+        else if (operation == "empty")
+        {
+            setBit = 0;
         }
     }
 
